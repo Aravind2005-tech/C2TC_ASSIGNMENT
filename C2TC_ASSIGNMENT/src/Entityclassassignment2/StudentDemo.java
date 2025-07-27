b20d6bcbe70f3685c1039c9f12a8709e3c4fa4f9
Aravind2005-tech <aravindmvit03@gmail.com>
2025-07-27 16:01:34 +0530

    pppp

--- C2TC_ASSIGNMENT/src/Entityclassassignment2/StudentDemo.java
+++ C2TC_ASSIGNMENT/src/Entityclassassignment2/StudentDemo.java
@@ -1,9 +1,11 @@
 package Entityclassassignment2;
 
+import entityclassassignment1.Student;
+
 public class StudentDemo {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		 Student s1 = new Student("Navin", 20, "IT");
+		
+		 Student s1 = new Student("Arvy", 20, "IT");
 
 	        // Calling method
 	        s1.displayDetails();
